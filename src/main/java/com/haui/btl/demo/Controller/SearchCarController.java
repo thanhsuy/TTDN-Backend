e0645ab08f79f0c7aa913ecab0957bf345089f05
thanh <thanhpsycho2k3@gmail.com>
2024-07-28 22:44:31 +0700

    fix bug 4

--- src/main/java/com/haui/btl/demo/Controller/SearchCarController.java
+++ src/main/java/com/haui/btl/demo/Controller/SearchCarController.java
@@ -6,10 +6,7 @@ import com.haui.btl.demo.dto.request.SearchCarRequest;
 import com.haui.btl.demo.dto.response.ApiResponse;
 import com.haui.btl.demo.dto.response.SearchCarResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +18,7 @@ public class SearchCarController {
     SearchCarService searchCarService;
 
 
-    @GetMapping
+    @PostMapping
     public ApiResponse<List<SearchCarResponse>> searchCar(@RequestBody SearchCarRequest searchCarRequest){
         return searchCarService.getListCar(searchCarRequest);
     }
