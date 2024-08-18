ac13fd1bed870a8feb240eb6e5b2d95469d81055
thanh <thanhpsycho2k3@gmail.com>
2024-08-18 21:37:57 +0700

    be final

--- src/main/java/com/haui/btl/demo/Repository/CarRepository.java
+++ src/main/java/com/haui/btl/demo/Repository/CarRepository.java
@@ -27,7 +27,7 @@ public interface CarRepository extends JpaRepository<Car, Integer>, CarRepositor
     @Query("SELECT c FROM Car c WHERE c.status <> 'Stopped' AND (c.status = 'Available' OR c.idcar NOT IN (" +
             "SELECT b.carIdcar FROM Booking b " +
             "WHERE b.startdatetime < :endTime AND b.enddatetime > :startTime " +
-            "AND b.status <> 'Completed')) " +
+            "AND b.status not in ('Completed', 'Cancelled'))) " +
             "AND c.address LIKE %:address%")
     List<Car> findAvailableCars(@Param("address") String address,
                                 @Param("startTime") LocalDateTime startTime,
@@ -37,7 +37,7 @@ public interface CarRepository extends JpaRepository<Car, Integer>, CarRepositor
             "SELECT 1 FROM car WHERE idCar = :idCar AND (status = 'Available' OR idCar NOT IN (" +
             "SELECT Car_idCar FROM booking " +
             "WHERE startDateTime < :endTime AND endDateTime > :startTime " +
-            "AND status <> 'Complete')))",
+            "AND status not in ('Completed', 'Cancelled')))",
             nativeQuery = true)
     Long checkCarAvailable(@Param("startTime") LocalDate startTime,
                            @Param("endTime") LocalDate endTime,
